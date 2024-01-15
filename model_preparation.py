11b51ccd39f2744f56cc2818fadaaa17f3d80563
Varsha <varshakt6@gmail.com>
2024-01-15 13:16:30 +0530

    model prepatation

---
+++ model_preparation.py
@@ -0,0 +1,276 @@
+# Import the required libraries.
+import os
+import cv2
+# import pafy
+import math
+import random
+import numpy as np
+import datetime as dt
+import tensorflow as tf
+from collections import deque
+# import matplotlib.pyplot as plt
+
+from moviepy.editor import *
+
+
+from sklearn.model_selection import train_test_split
+
+from tensorflow.keras.layers import *
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.utils import plot_model
+
+seed_constant = 27
+np.random.seed(seed_constant)
+random.seed(seed_constant)
+tf.random.set_seed(seed_constant)
+
+# Specify the height and width to which each video frame will be resized in our dataset.
+IMAGE_HEIGHT , IMAGE_WIDTH = 64, 64
+
+# Specify the number of frames of a video that will be fed to the model as one sequence.
+SEQUENCE_LENGTH = 20
+
+# Specify the directory containing the UCF50 dataset. 
+DATASET_DIR = "MainDataset/UCF-101"
+
+
+# List all folders within the parent directory
+# folder_names = [folder for folder in os.listdir(DATASET_DIR) if os.path.isdir(os.path.join(DATASET_DIR, folder))]
+
+# # # Print the folder names
+# for folder_name in folder_names:
+#     print(folder_name)
+
+# Specify the list containing the names of the classes used for training. Feel free to choose any set of classes.
+# CLASSES_LIST = folder_names
+
+CLASSES_LIST = ["WalkingWithDog", "TaiChi", "Swing", "HorseRace"]
+
+
+# Create a Function to Extract, Resize & Normalize Frames
+
+# We will create a function frames_extraction() that will create a list containing the resized and normalized frames of a video 
+# whose path is passed to it as an argument. The function will read the video file frame by frame, 
+# although not all frames are added to the list as we will only need an evenly distributed sequence length of frames.
+
+def frames_extraction(video_path):
+    '''
+    This function will extract the required frames from a video after resizing and normalizing them.
+    Args:
+        video_path: The path of the video in the disk, whose frames are to be extracted.
+    Returns:
+        frames_list: A list containing the resized and normalized frames of the video.
+    '''
+
+    # Declare a list to store video frames.
+    frames_list = []
+    
+    # Read the Video File using the VideoCapture object.
+    video_reader = cv2.VideoCapture(video_path)
+
+    # Get the total number of frames in the video.
+    video_frames_count = int(video_reader.get(cv2.CAP_PROP_FRAME_COUNT))
+
+    # Calculate the the interval after which frames will be added to the list.
+    skip_frames_window = max(int(video_frames_count/SEQUENCE_LENGTH), 1)
+
+    # Iterate through the Video Frames.
+    for frame_counter in range(SEQUENCE_LENGTH):
+
+        # Set the current frame position of the video.
+        video_reader.set(cv2.CAP_PROP_POS_FRAMES, frame_counter * skip_frames_window)
+
+        # Reading the frame from the video. 
+        success, frame = video_reader.read() 
+
+        # Check if Video frame is not successfully read then break the loop
+        if not success:
+            break
+
+        # Resize the Frame to fixed height and width.
+        resized_frame = cv2.resize(frame, (IMAGE_HEIGHT, IMAGE_WIDTH))
+        
+        # Normalize the resized frame by dividing it with 255 so that each pixel value then lies between 0 and 1
+        normalized_frame = resized_frame / 255
+        
+        # Append the normalized frame into the frames list
+        frames_list.append(normalized_frame)
+    
+    # Release the VideoCapture object. 
+    video_reader.release()
+
+    # Return the frames list.
+    return frames_list
+
+# Create a Function for Dataset Creation
+
+# Now we will create a function create_dataset() that will iterate through all the classes specified in the CLASSES_LIST constant and 
+# will call the function frame_extraction() on every video file of the selected classes and 
+# return the frames (features), class index ( labels), and video file path (video_files_paths).
+def create_dataset():
+    '''
+    This function will extract the data of the selected classes and create the required dataset.
+    Returns:
+        features:          A list containing the extracted frames of the videos.
+        labels:            A list containing the indexes of the classes associated with the videos.
+        video_files_paths: A list containing the paths of the videos in the disk.
+    '''
+
+    # Declared Empty Lists to store the features, labels and video file path values.
+    features = []
+    labels = []
+    video_files_paths = []
+    
+    # Iterating through all the classes mentioned in the classes list
+    for class_index, class_name in enumerate(CLASSES_LIST):
+        
+        # Display the name of the class whose data is being extracted.
+        print(f'Extracting Data of Class: {class_name}')
+        
+        # Get the list of video files present in the specific class name directory.
+        files_list = os.listdir(os.path.join(DATASET_DIR, class_name))
+        
+        # Iterate through all the files present in the files list.
+        for file_name in files_list:
+            
+            # Get the complete video path.
+            video_file_path = os.path.join(DATASET_DIR, class_name, file_name)
+
+            # Extract the frames of the video file.
+            frames = frames_extraction(video_file_path)
+
+            # Check if the extracted frames are equal to the SEQUENCE_LENGTH specified above.
+            # So ignore the vides having frames less than the SEQUENCE_LENGTH.
+            if len(frames) == SEQUENCE_LENGTH:
+
+                # Append the data to their repective lists.
+                features.append(frames)
+                labels.append(class_index)
+                video_files_paths.append(video_file_path)
+
+    # Converting the list to numpy arrays
+    features = np.asarray(features)
+    labels = np.array(labels)  
+    
+    # Return the frames, class index, and video file path.
+    return features, labels, video_files_paths
+
+
+# Create the dataset.
+features, labels, video_files_paths = create_dataset()
+
+
+# Using Keras's to_categorical method to convert labels into one-hot-encoded vectors
+one_hot_encoded_labels = to_categorical(labels)
+
+
+
+# Split the Data into Train ( 75% ) and Test Set ( 25% ).
+features_train, features_test, labels_train, labels_test = train_test_split(features, one_hot_encoded_labels,
+                                                                            test_size = 0.25, shuffle = True,
+                                                                            random_state = seed_constant)
+
+
+def create_LRCN_model():
+    '''
+    This function will construct the required LRCN model.
+    Returns:
+        model: It is the required constructed LRCN model.
+    '''
+
+    # We will use a Sequential model for model construction.
+    model = Sequential()
+    
+    # Define the Model Architecture.
+    ########################################################################################################################
+    
+    model.add(TimeDistributed(Conv2D(16, (3, 3), padding='same',activation = 'relu'),
+                              input_shape = (SEQUENCE_LENGTH, IMAGE_HEIGHT, IMAGE_WIDTH, 3)))
+    
+    model.add(TimeDistributed(MaxPooling2D((4, 4)))) 
+    model.add(TimeDistributed(Dropout(0.25)))
+    
+    model.add(TimeDistributed(Conv2D(32, (3, 3), padding='same',activation = 'relu')))
+    model.add(TimeDistributed(MaxPooling2D((4, 4))))
+    model.add(TimeDistributed(Dropout(0.25)))
+    
+    model.add(TimeDistributed(Conv2D(64, (3, 3), padding='same',activation = 'relu')))
+    model.add(TimeDistributed(MaxPooling2D((2, 2))))
+    model.add(TimeDistributed(Dropout(0.25)))
+    
+    model.add(TimeDistributed(Conv2D(64, (3, 3), padding='same',activation = 'relu')))
+    model.add(TimeDistributed(MaxPooling2D((2, 2))))
+    #model.add(TimeDistributed(Dropout(0.25)))
+                                      
+    model.add(TimeDistributed(Flatten()))
+                                      
+    model.add(LSTM(32))
+                                      
+    model.add(Dense(len(CLASSES_LIST), activation = 'softmax'))
+
+    ########################################################################################################################
+
+    # Display the models summary.
+    model.summary()
+    
+    # Return the constructed LRCN model.
+    return model
+
+
+
+
+
+# Construct the required LRCN model.
+LRCN_model = create_LRCN_model()
+
+# Display the success message.
+print("Model Created Successfully!")
+
+
+
+
+
+# Create an Instance of Early Stopping Callback.
+early_stopping_callback = EarlyStopping(monitor = 'val_loss', patience = 15, mode = 'min', restore_best_weights = True)
+ 
+# Compile the model and specify loss function, optimizer and metrics to the model.
+LRCN_model.compile(loss = 'categorical_crossentropy', optimizer = 'Adam', metrics = ["accuracy"])
+
+# Start training the model.
+LRCN_model_training_history = LRCN_model.fit(x = features_train, y = labels_train, epochs = 100, batch_size = 8 ,
+                                             shuffle = True, validation_split = 0.2, callbacks = [early_stopping_callback])
+
+
+
+# Evaluate the trained model.
+model_evaluation_history = LRCN_model.evaluate(features_test, labels_test)
+
+
+# Get the loss and accuracy from model_evaluation_history.
+model_evaluation_loss, model_evaluation_accuracy = model_evaluation_history
+
+# Define the string date format.
+# Get the current Date and Time in a DateTime Object.
+# Convert the DateTime object to string according to the style mentioned in date_time_format string.
+date_time_format = '%Y_%m_%d__%H_%M_%S'
+current_date_time_dt = dt.datetime.now()
+current_date_time_string = dt.datetime.strftime(current_date_time_dt, date_time_format)
+    
+# Define a useful name for our model to make it easy for us while navigating through multiple saved models.
+model_file_name = f'LRCN_model___Date_Time_{current_date_time_string}___Loss_{model_evaluation_loss}___Accuracy_{model_evaluation_accuracy}.h5'
+
+
+# Make the Output directory if it does not exist
+model_directory = 'SavedModel'
+os.makedirs(model_directory, exist_ok = True)
+
+# Save the Model.
+LRCN_model.save(os.path.join(model_directory,model_file_name))
+
+# # Save the Model.
+# LRCN_model.save(model_file_name)
+
+
+
---
+++ predict.py
@@ -0,0 +1,109 @@
+import os
+import cv2
+# import pafy
+import math
+import random
+import numpy as np
+import datetime as dt
+import tensorflow as tf
+from collections import deque
+# import matplotlib.pyplot as plt
+from tensorflow.keras.models import load_model
+from moviepy.editor import *
+
+# Specify the height and width to which each video frame will be resized in our dataset.
+IMAGE_HEIGHT , IMAGE_WIDTH = 64, 64
+
+# Specify the number of frames of a video that will be fed to the model as one sequence.
+SEQUENCE_LENGTH = 20
+
+# Specify the directory containing the UCF50 dataset. 
+DATASET_DIR = "MainDataset/UCF-101"
+
+# Specify the list containing the names of the classes used for training. Feel free to choose any set of classes.
+CLASSES_LIST = ["WalkingWithDog", "TaiChi", "Swing", "HorseRace"]
+
+# Load the saved model
+loaded_model = load_model('savedModel/LRCN_model___Date_Time_2024_01_13__18_38_04___Loss_0.37888357043266296___Accuracy_0.8583333492279053.h5')
+
+
+
+def predict_on_video(video_file_path, output_file_path, SEQUENCE_LENGTH):
+    '''
+    This function will perform action recognition on a video using the LRCN model.
+    Args:
+    video_file_path:  The path of the video stored in the disk on which the action recognition is to be performed.
+    output_file_path: The path where the ouput video with the predicted action being performed overlayed will be stored.
+    SEQUENCE_LENGTH:  The fixed number of frames of a video that can be passed to the model as one sequence.
+    '''
+
+    # Initialize the VideoCapture object to read from the video file.
+    video_reader = cv2.VideoCapture(video_file_path)
+
+    # Get the width and height of the video.
+    original_video_width = int(video_reader.get(cv2.CAP_PROP_FRAME_WIDTH))
+    original_video_height = int(video_reader.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    # Initialize the VideoWriter Object to store the output video in the disk.
+    video_writer = cv2.VideoWriter(output_file_path, cv2.VideoWriter_fourcc('M', 'P', '4', 'V'), 
+                                   video_reader.get(cv2.CAP_PROP_FPS), (original_video_width, original_video_height))
+
+    # Declare a queue to store video frames.
+    frames_queue = deque(maxlen = SEQUENCE_LENGTH)
+
+    # Initialize a variable to store the predicted action being performed in the video.
+    predicted_class_name = ''
+
+    # Iterate until the video is accessed successfully.
+    while video_reader.isOpened():
+
+        # Read the frame.
+        ok, frame = video_reader.read() 
+        
+        # Check if frame is not read properly then break the loop.
+        if not ok:
+            break
+
+        # Resize the Frame to fixed Dimensions.
+        resized_frame = cv2.resize(frame, (IMAGE_HEIGHT, IMAGE_WIDTH))
+        
+        # Normalize the resized frame by dividing it with 255 so that each pixel value then lies between 0 and 1.
+        normalized_frame = resized_frame / 255
+
+        # Appending the pre-processed frame into the frames list.
+        frames_queue.append(normalized_frame)
+
+        # Check if the number of frames in the queue are equal to the fixed sequence length.
+        if len(frames_queue) == SEQUENCE_LENGTH:
+
+            # Pass the normalized frames to the model and get the predicted probabilities.
+            predicted_labels_probabilities = loaded_model.predict(np.expand_dims(frames_queue, axis = 0))[0]
+
+            # Get the index of class with highest probability.
+            predicted_label = np.argmax(predicted_labels_probabilities)
+
+            # Get the class name using the retrieved index.
+            predicted_class_name = CLASSES_LIST[predicted_label]
+
+        # Write predicted class name on top of the frame.
+        cv2.putText(frame, predicted_class_name, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
+        # Write The frame into the disk using the VideoWriter Object.
+        video_writer.write(frame)
+        
+    # Release the VideoCapture and VideoWriter objects.
+    video_reader.release()
+    video_writer.release()
+
+# Make the Output directory if it does not exist
+test_videos_directory = 'test_videos'
+
+# Get the YouTube Video's path we just downloaded.
+input_video_file_path = f'{test_videos_directory}/sample.mp4'
+
+
+# Construct the output video path.
+output_video_file_path = f'{test_videos_directory}/sample-Output-SeqLen{SEQUENCE_LENGTH}.mp4'
+
+# Perform Action Recognition on the Test Video.
+predict_on_video(input_video_file_path, output_video_file_path, SEQUENCE_LENGTH)
